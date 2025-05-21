19edf8ccd3fe91472fffa83f8f9ca96366d170e7
attiqa420 <attiqaj2@gmail.com>
2025-05-21 06:57:06 +0500

    Updates

---
+++ precache-manifest.50ff8df78f8ba436218991ab460311c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a18c565d123acd6feb31383173f0341",
+    "url": "/memes-generator/index.html"
+  },
+  {
+    "revision": "07cfc6ff866c1580c540",
+    "url": "/memes-generator/static/css/main.a25e663c.chunk.css"
+  },
+  {
+    "revision": "d462ed01062ad80991b7",
+    "url": "/memes-generator/static/js/2.1a0df897.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/memes-generator/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "07cfc6ff866c1580c540",
+    "url": "/memes-generator/static/js/main.23f1185f.chunk.js"
+  },
+  {
+    "revision": "e2f50a74370ec64b2b74",
+    "url": "/memes-generator/static/js/runtime~main.290c507d.js"
+  }
+]);
